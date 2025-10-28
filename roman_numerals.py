951e4a5231c44751fe73305714169fc17f5c2138
rvillarrealcp <richard.villarreal2@hotmail.com>
2025-10-28 15:49:32 -0500

    Assignment complete

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,3 +1,27 @@
 def to_roman(num):
-    # write your code here!
-    pass
+    #create dictionary for roman numerals
+    romans = {
+        "M" : 1000,
+        "CM" : 900,
+        "D" : 500,
+        "C" : 100,
+        "L" : 50,
+        "XL" : 40,
+        "X" : 10,
+        "IX": 9,
+        "V" : 5,
+        "IV": 4,
+        "I" : 1
+    }
+    result = []
+    while num > 0:
+        for key, val in romans.items():
+            if val <= num:
+                num -= val
+                result.append(key)
+                break
+    return ''.join(result)
+
+
+# print(to_roman(5254))
+
